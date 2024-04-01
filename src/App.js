3221d733cf342b62d58a799193e3b7d621f69f81
Priyank Raj <priyank@copartner.in>
2024-04-01 17:58:16 +0530

    finial_push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import HomePage from './components/HomePage';
-import HomePageTwo from './components/HomePageTwo';
 
 function App() {
   return (
     <div className="bg-[#06030E] overflow-hidden">
       <HomePage />
-      {/* <HomePageTwo /> */}
     </div>
   );
 }
